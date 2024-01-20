691b7a3eecedf8c9aa7f231ee01fbe068362fb9c
99hyejin <jinatpc@gmail.com>
2024-01-21 05:22:49 +0900

    backend

---
+++ types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "afarmaf6dd713": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "S3Trigger116366b4": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "afarms39727fdce": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
